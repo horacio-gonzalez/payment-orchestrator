8df65cd53239b70b9898513ce1fdd31b7033035c
Horacio Gonzalez <horaciojesusgg@gmail.com>
2026-01-19 15:27:31 -0300

    feat: Integrate @nestjs/config and Joi for validation
    Add @nestjs/config and joi to the project. This allows for global
    configuration management and environment variable validation using Joi
    schemas.
    
    Also, update the .gitignore to include .env files and add new scripts
    for Knex migrations and seeds to package.json.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,41 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsModule } from './modules/payments/payments.module';
 import { WebhooksModule } from './modules/webhooks/webhooks.module';
+import { DatabaseModule } from './shared/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [PaymentsModule, WebhooksModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        // Server
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        // Database (Postgres)
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+
+        // Redis (Para BullMQ)
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().default(6379),
+      }),
+      // Evita que la app arranque si falta algo crítico
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+    DatabaseModule,
+    PaymentsModule,
+    WebhooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
